3bae5055dd5ee4f53fb52b306198984c9535b689
Monica <monica.guajardo@oportun.com>
2022-10-05 15:34:23 -0500

    Modfied exercise to fit use-case

--- beautifulSoup/Mexico_population.py
+++ beautifulSoup/Mexico_population.py
@@ -2,9 +2,6 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import requests
 
-population_url = "https://en.wikipedia.org/wiki/List_of_cities_in_Mexico"
-target_class = "wikitable sortable jquery-tablesorter"
-
 response = requests.get(url = "https://en.wikipedia.org/wiki/List_of_cities_in_Mexico")
 print(response.status_code)
 
@@ -12,8 +9,8 @@ soup = BeautifulSoup(response.text, 'html.parser')
 cities_by_population = soup.find_all('table',{'class':"wikitable"})[1]
 
 cities_df=pd.read_html(str(cities_by_population))
-
 cities_df=pd.DataFrame(cities_df[0])
+cities_df.to_csv('Mexico_population_3.csv')
+
 print(cities_df.head(2))
 
-cities_df.to_csv('Mexico_population.csv')
