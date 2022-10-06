ad819c837790f17ecc13be11f795a794d38a56dd
Monica <monica.guajardo@oportun.com>
2022-10-06 14:30:59 -0500

    Add complete student exercise

--- Scrapy/bookstore/bookstore/spiders/booky.py
+++ Scrapy/bookstore/bookstore/spiders/booky.py
@@ -10,20 +10,28 @@ class BookySpider(scrapy.Spider):
 
     def parse(self, response):
         items = BookstoreItem()
-        titles = response.css("h3 a::text").extract()
+        titles = response.xpath('//h3/a/text()').extract()
         ratings = response.xpath("//article/p/@class").extract()
+        price = response.xpath('//div/p[@class="price_color"]/text()').extract()
         stocks = response.xpath('//div/p[@class="instock availability"]/text()').extract()
         links = response.xpath('//h3/a/@href').extract()
         
+        #Transform ratings and links
+        ratings = [rating.replace("star-rating","") for rating in ratings]
+        links = [f"http://books.toscrape.com/{link}" for link in links] 
         
+        zip_info = zip(titles, ratings, price, stocks, links)
+        info_list = list(zip_info)
         
-    
-        
-        item['title'] = title
-        item['rating'] = rating
-        item['price'] = price
-        item['stock'] = stock
-        item['link'] = link
+        for row in info_list:
+            title, rating, price, stock, link = row
+            items['title'] = title
+            items['rating'] = rating
+            items['price'] = str(price)
+            items['stock'] = stock
+            items['link'] = link
+            
+            yield items
         
         
         
