719fcf95572b9477c8fe72d0a5f3d758c12249db
Monica <monica.guajardo@oportun.com>
2022-10-06 12:40:06 -0500

    Add selectors

--- Scrapy/bookstore/bookstore/items.py
+++ Scrapy/bookstore/bookstore/items.py
@@ -1,15 +1,11 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
 
 
 class BookstoreItem(scrapy.Item):
-    link = scrapy.Field()
     title = scrapy.Field()
     price = scrapy.Field()
     stock = scrapy.Field()
     rating = scrapy.Field()
+    link = scrapy.Field()
     
--- Scrapy/bookstore/bookstore/spiders/booky.py
+++ Scrapy/bookstore/bookstore/spiders/booky.py
@@ -9,4 +9,21 @@ class BookySpider(scrapy.Spider):
     start_urls = ['http://books.toscrape.com/index.html']
 
     def parse(self, response):
-        pass
+        items = BookstoreItem()
+        titles = response.css("h3 a::text").extract()
+        ratings = response.xpath("//article/p/@class").extract()
+        stocks = response.xpath('//div/p[@class="instock availability"]/text()').extract()
+        links = response.xpath('//h3/a/@href').extract()
+        
+        
+        
+    
+        
+        item['title'] = title
+        item['rating'] = rating
+        item['price'] = price
+        item['stock'] = stock
+        item['link'] = link
+        
+        
+        
